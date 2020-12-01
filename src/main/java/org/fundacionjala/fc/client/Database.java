5f9cf8f6d61838aed35158a343ad9e09385b66da
Samuel Loza <starsaminf@gmail.com>
2020-12-01 16:22:21 -0400

    Add try/catch to Database.java file

--- src/main/java/org/fundacionjala/fc/client/Database.java
+++ src/main/java/org/fundacionjala/fc/client/Database.java
@@ -1,10 +1,11 @@
 package org.fundacionjala.fc.client;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.fundacionjala.fc.config.Environment;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
@@ -16,15 +17,20 @@ public class Database {
     private static final String DB_URL = Environment.getInstance().getDbMysqlUrlConection();
     private static final String DB_USERNAME = Environment.getInstance().getDbUserName();
     private static final String DB_PWD = Environment.getInstance().getDbPassword();
+    private static final Logger LOGGER = LogManager.getLogger(org.mozilla.javascript.tools.shell.Environment.class);
+
     private Connection dbConnection;
 
     /**
      * Constructor for the Database.
-     *
-     * @throws SQLException
      */
-    public Database() throws SQLException {
-        dbConnection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PWD);
+    public Database() {
+        try {
+            dbConnection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PWD);
+        } catch (Exception e) {
+            LOGGER.error("Error when connecting to database");
+            LOGGER.error(e.getMessage());
+        }
     }
 
     /**
@@ -32,20 +38,24 @@ public class Database {
      *
      * @param query
      * @return query result as Map<String, String>
-     * @throws SQLException
      */
-    public Map<String, String> query(final String query) throws SQLException {
-        Statement stmt = dbConnection.createStatement();
-        ResultSet rs = stmt.executeQuery(query);
-        ResultSetMetaData metaData = rs.getMetaData();
-        int columns = metaData.getColumnCount();
+    public Map<String, String> query(final String query) {
         Map<String, String> results = new HashMap<>();
-        while (rs.next()) {
-            for (int i = 1; i <= columns; i++) {
-                String key = metaData.getColumnName(i);
-                String value = rs.getString(i);
-                results.put(key, value);
+        try {
+            Statement stmt = dbConnection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            ResultSetMetaData metaData = rs.getMetaData();
+            int columns = metaData.getColumnCount();
+            while (rs.next()) {
+                for (int i = 1; i <= columns; i++) {
+                    String key = metaData.getColumnName(i);
+                    String value = rs.getString(i);
+                    results.put(key, value);
+                }
             }
+        } catch (Exception e) {
+            LOGGER.error("Error when executing query");
+            LOGGER.error(e.getMessage());
         }
         return results;
     }
